c7acb7fe790a0375b442a2aae2aae82cf37b48e8
TDhananjaya <tharindudhananjaya1122@gmail.com>
2018-02-04 18:08:00 +0530

    GUI for cal

---
+++ CalculatorGUI/src/calculatorgui/CalculatorGUI.java
@@ -0,0 +1,15 @@
+
+package calculatorgui;
+
+/**
+ *
+ * @author Dhana
+ */
+public class CalculatorGUI {
+
+ 
+    public static void main(String[] args) {
+         Calculator c=new Calculator();
+         c.setVisible(true);
+    }
+}
